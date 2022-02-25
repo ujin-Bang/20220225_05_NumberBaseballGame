98b7fdc8bf95c77fa70a19b14a2bfb53935e8999
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-02-25 15:45:36 +0900

    중복된 숫자가 아니라면 문제로 선정하기

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -18,7 +18,28 @@ public class MainDrive {
 				Random myRandom = new Random();
 				int randomNum = myRandom.nextInt(9)+1; //0 ~ 10직전까지 나옴(9까지)
 				
+				boolean isDuplOk = true;
+				
+				for(int num: cpuNumbers) {
+					if(num == randomNum) {
+						isDuplOk = false;
+						break;
+					}
+				}
+				
+				if(isDuplOk) {
+					cpuNumbers[i] = randomNum;
+					break;
+				}
 			}
 		}
+		
+		
+//		문제 확인용 for문
+		for(int num: cpuNumbers) {
+			System.out.println(num);
+		}
+		
+		
 	}
 }
